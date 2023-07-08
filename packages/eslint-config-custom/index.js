cb4eb291f9794a0f443bb4fbd576bde4989b3746
darekaze <darekaze@gmail.com>
2023-07-08 18:12:33 +0800

    chore: convert back to nextjs

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,12 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "turbo",
-    "prettier",
-  ],
+  extends: ["next/core-web-vitals", "turbo", "prettier"],
   rules: {
+    "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
 }
